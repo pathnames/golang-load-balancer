d9d7b5f826ccd92720152737acfb77c8441ac238
pathnames <pathnames@users.noreply.github.com>
2025-11-15 19:31:35 -0500

    Created helper functions to set a backend to be alive, and to check whether a backend server is alive or not.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"sync"
+	"sync/atomic"
 )
 
 // Data structure to represent a backend server.
@@ -19,6 +20,25 @@ type Backend struct {
 type ServerPool struct {
 	backends []*Backend
 	current uint64
+}
+
+func (s *ServerPool) NextIndex() int {
+  return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
+}
+
+// Sets backend to be alive.
+func (b *Backend) SetAlive(alive bool) {
+	b.mux.Lock()
+	b.Alive = alive
+	b.mux.Unlock()
+}
+
+// Helper function to check whether a backend is alive or not.
+func (b *Backend) IsAlive() (alive bool) {
+	b.mux.RLock()
+	alive = b.Alive
+	b.mux.RUnlock()
+	return
 }
 
 func main() {
