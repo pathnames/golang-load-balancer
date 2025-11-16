e88053c8fd26ea08d09b18bc55470ef30aa75863
pathnames <pathnames@users.noreply.github.com>
2025-11-15 19:40:24 -0500

    Added helpful comment for why NextIndex() needs an atomic increment.

--- main.go
+++ main.go
@@ -22,6 +22,17 @@ type ServerPool struct {
 	current uint64
 }
 
+// Returns the next index in the backends slice to route the request to. 
+// modulus trick is used to keep index in the range [0, len(backends) - 1]
+// Imagine multiple requests are happening at the same time (common in a server).
+// Without atomic, two requests could run NextIndex() at the exact same time.
+// Both read the same current value before either increments it.
+// Both could return the same index, even though they should pick different servers.
+// This would break the round-robin behavior.
+// atomic.AddUint64 guarantees that the increment happens safely, even if many requests happen at the same time.
+// No two requests will ever get the same increment value.
+// Each request sees a unique current value.
+// The round-robin works correctly under concurrent access.
 func (s *ServerPool) NextIndex() int {
   return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
 }
